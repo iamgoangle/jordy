e80781e227072b567501baeb1a7689b31503abb5
Teerapong Singthong <st.teerapong@gmail.com>
2020-02-29 01:43:25 +0700

    feat: init project

---
+++ src/views/Jordy.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="jordy">
+    <h1>This is Jordy page</h1>
+    <jordy />
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Jordy from "@/components/Jordy.vue";
+
+export default Vue.extend({
+  name: "JordyView",
+  components: {
+    Jordy
+  }
+});
+</script>
