e26b8dc5f218866338b26dcba2cfcf77b903d4b2
Teerapong Singthong <st.teerapong@gmail.com>
2020-02-29 14:14:19 +0700

    feat: add prop

---
+++ src/components/InputBox.vue
@@ -0,0 +1,38 @@
+<template>
+  <form>
+    <div class="form-group">
+      <h1>
+        <slot></slot>
+      </h1>
+      <label>Input your name</label>
+      <input type="text" class="form-control" :placeholder="hint" />
+    </div>
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "InputBox",
+  props: {
+    value: {
+      type: String
+    },
+    hint: {
+      type: String
+    }
+  },
+  data() {
+    return {
+      fullname: ""
+    };
+  },
+  methods: {
+    saveName() {
+      this.$emit("createName", this.fullname);
+    }
+  }
+});
+</script>
--- src/views/Jordy.vue
+++ src/views/Jordy.vue
@@ -2,17 +2,20 @@
   <div class="jordy">
     <h1>This is Jordy page</h1>
     <jordy />
+    <input-box hint="please enter your name..." />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
 import Jordy from "@/components/Jordy.vue";
+import InputBox from "@/components/InputBox.vue";
 
 export default Vue.extend({
   name: "JordyView",
   components: {
-    Jordy
+    Jordy,
+    InputBox
   }
 });
 </script>
